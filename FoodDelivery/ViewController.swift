5424f7f2f5525aa4f878bea7d5c53fa63900f16d
Ganesh Somani <ganesh.somani@gmail.com>
2021-04-17 11:38:44 +0530

    Empty project added

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Ganesh Somani on 17/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
